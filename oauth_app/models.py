51d8edd959539ca22ee5d8e6d4a892963df788cd
Satendra <satendra.raj1241@gmail.com>
2022-07-16 22:30:12 +0200

    cascade

--- oauth_app/models.py
+++ oauth_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Rating(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     rating = models.IntegerField(default=1000)
     k = models.IntegerField(default=30)
 
